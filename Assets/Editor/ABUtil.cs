3c918eaeeb1f80d33817123b80c7c3e86b021ce7
icnbpLBC <icnbperfect2015@163.com>
2022-12-20 17:39:54 +0800

    feature: 生成AB包的版本文件

--- Assets/Editor/ABUtil.cs
+++ Assets/Editor/ABUtil.cs
@@ -6,14 +6,39 @@ using System.IO;
 
 public class ABUtil : Editor
 {
+    static string path = "Assets/ArtRes/AB";
+
     [MenuItem("Tools/CreateAssetBundle")]
     public static void CreatePCAB()
     {
-        string path = "Assets/ArtRes/AB";
         if (!Directory.Exists(path)){
             Directory.CreateDirectory(path);
         }
         BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
         UnityEngine.Debug.Log("win finish create ab");
+    }
+
+    [MenuItem("Tools/CreateABVersionFile")]
+    public static void CreateABVersionFile()
+    {
+        DirectoryInfo directory = Directory.CreateDirectory(path);
+        FileInfo[] fileInfos = directory.GetFiles();
+        string abVersionInfo = "";
+
+        foreach(FileInfo info in fileInfos)
+        {   
+            // 根据后缀判断是否AB包
+            if(info.Extension == "")
+            {   
+                // 每个ab包资源均为 包名_日期
+                abVersionInfo += info.Name + "_" + System.DateTime.Now.ToString("d");
+                // 空格分割
+                abVersionInfo += " ";
+            }
+        }
+        // 去掉最后的分隔符
+        abVersionInfo = abVersionInfo.Substring(0, abVersionInfo.Length - 1);
+        // 写入版本文件
+        File.WriteAllText(path + "/ab_version.txt", abVersionInfo);
     }
 }
